d35ca8888fa5641b0cd7013316b82d926e354309
EegleZ <maksstyzej@gmail.com>
2024-06-27 22:32:43 +0200

    feat: simple button menu

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,70 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import ButtonLabel from '@/components/ButtonLabel';
+import { StyleSheet, Text, Alert, Image } from 'react-native';
+import { GestureHandlerRootView, TouchableOpacity } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <GestureHandlerRootView>
+      <SafeAreaView style={{flex: 1}}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => Alert.alert('Przenieś rower')}
+        >
+          <ButtonLabel
+            text="Przenieś rower"
+            source={require('@/assets/images/move.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => Alert.alert('Złóż rower')}
+        >
+          <ButtonLabel
+            text="Złóż rower"
+            source={require('@/assets/images/unpack.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => Alert.alert('Sprzedaj rower')}
+        >
+          <ButtonLabel
+            text="Sprzedaj rower"
+            source={require('@/assets/images/sell.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => Alert.alert('Dodaj rower')}
+        >
+          <ButtonLabel
+            text="Dodaj rower"
+            source={require('@/assets/images/plus.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => Alert.alert('Dostawa')}
+        >
+          <ButtonLabel
+            text="Dostawa"
+            source={require('@/assets/images/delivery.png')}
+          />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </GestureHandlerRootView>
+
+  )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  button: {
+    height:100,
+    backgroundColor: '#78E0FF',
+    textAlignVertical: "center",
     flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
   },
 });
---
+++ components/ButtonLabel.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, View, Image, Text } from 'react-native'
+
+
+type LabelProps = {
+  text: string;
+  source: any;
+}
+const ButtonLabel = (props: LabelProps) => {
+  return (
+    <View style={styles.label}>
+      <Image style={styles.icon} source={props.source}/>
+      <Text style={styles.buttonDesc}>{props.text}</Text>
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  buttonDesc: {
+    fontSize: 30,
+    flex:4,
+    color: '#FFFFFF'
+  },
+  icon: {
+    resizeMode: 'contain',
+    height: 50,
+    flex:1,
+  },
+  label: {
+    textAlignVertical: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+
+export default ButtonLabel;
