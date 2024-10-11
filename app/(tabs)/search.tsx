48b80abfda79bfbe81b61c66dfe4ab4f7ff0eea1
makrowave <makrowave@netpanel-87-246-222-25.pol.akademiki.lublin.pl>
2024-10-11 17:10:11 +0200

    mend

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import { ThemedText } from "@/components/ThemedText";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function SearchScreen() {
+    return (
+        <GestureHandlerRootView>
+            <SafeAreaView>
+                
+            </SafeAreaView>
+        </GestureHandlerRootView>
+    )
+}
