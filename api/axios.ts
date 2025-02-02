7413c814a2e67f4ad09e84419646e301de78d89e
makrowave <maksstyzej@gmail.com>
2025-02-02 13:26:50 +0100

    refactor: moved api url to .env

--- api/axios.ts
+++ api/axios.ts
@@ -1,15 +1,14 @@
 import axios from "axios";
+import Constants from "expo-constants";
+
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export default axios.create({
-  baseURL: "http://192.168.1.101:5001/api",
-  // baseURL: "http://192.168.1.50:5001/api",
-  // baseURL: "http://172.20.10.9:5001/api",
+  baseURL: API_URL,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "http://192.168.1.101:5001/api",
-  // baseURL: "http://192.168.1.50:5001/api",
-  // baseURL: "http://172.20.10.9:5001/api",
+  baseURL: API_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
